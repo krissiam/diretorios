5c04d146f906aef844692cbfee6a3b212922cdbe
Krissia Menezes <krissiamlm@gmail.com>
2017-06-07 08:35:05 -0300

    Verifica se o acesso vem de um dispositivo Android

---
+++ verifica-android.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <!--Verifica se o acesso vem de um dispositivo Android-->
+    <title>Verifica acesso</title>
+    <script type='text/javascript'>
+        if ((navigator.userAgent.indexOf('Android') != -1)){
+            alert('Android!!!');
+        }
+        else{
+            alert('Não é Android!!!');
+        }
+
+
+    </script>
+</head>
+<body>
+</body>
+</html>
